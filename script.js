7e181c50cd23db7c5bac57bb810969680e757807
Rakesh Roshan <roshanrakesh7362@gmail.com>
2024-01-10 03:37:09 +0530

    Files Formatted

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* ----- Preloader ----- */
 
 {
-    window.addEventListener('load', function() {
+    window.addEventListener('load', function () {
         var preloader = document.getElementById('loading');
         preloader.style.display = 'none';
     });
@@ -22,19 +22,19 @@
     document.addEventListener("DOMContentLoaded", function () {
         const menuItems = document.querySelectorAll(".menu-btn");
         const sections = document.querySelectorAll("section");
-    
+
         window.addEventListener("scroll", function () {
             let current = "";
-    
+
             sections.forEach(section => {
                 const sectionTop = section.offsetTop;
                 const sectionHeight = section.clientHeight;
-    
+
                 if (scrollY >= sectionTop - sectionHeight / 7) {
                     current = section.getAttribute("id");
                 }
             });
-    
+
             menuItems.forEach(item => {
                 item.classList.remove("active");
                 if (item.getAttribute("href").slice(1) === current) {
@@ -49,10 +49,10 @@
 
 {
     var typed = new Typed(".typing", {
-        strings:["","A C++ Coder","Web Developer","Open Source Enthusiast","into Machine Learning"],
-        typeSpeed:100,
-        BackSpeed:60,
-        loop:true
+        strings: ["", "A C++ Coder", "Web Developer", "Open Source Enthusiast", "into Machine Learning"],
+        typeSpeed: 100,
+        BackSpeed: 60,
+        loop: true
     });
 }
 
@@ -61,16 +61,16 @@
 {
     const coords = { x: 0, y: 0 };
     const circles = document.querySelectorAll(".circle");
-    
-    const colors = [ "#ffb56b", "#fdaf69", "#f89d63", "#f59761", "#ef865e", "#ec805d", "#e36e5c", "#df685c", "#d5585c", "#d1525c", "#c5415d", "#c03b5d", "#b22c5e", "#ac265e", "#9c155f", "#950f5f", "#830060", "#7c0060", "#680060", "#60005f", "#48005f", "#3d005e" ];
-    
+
+    const colors = ["#ffb56b", "#fdaf69", "#f89d63", "#f59761", "#ef865e", "#ec805d", "#e36e5c", "#df685c", "#d5585c", "#d1525c", "#c5415d", "#c03b5d", "#b22c5e", "#ac265e", "#9c155f", "#950f5f", "#830060", "#7c0060", "#680060", "#60005f", "#48005f", "#3d005e"];
+
     circles.forEach(function (circle, index) {
         circle.x = 0;
         circle.y = 0;
         circle.style.backgroundColor = colors[index % colors.length];
     });
-    
-    window.addEventListener("mousemove", function(e) {
+
+    window.addEventListener("mousemove", function (e) {
         coords.x = e.clientX;
         coords.y = e.clientY;
     });
@@ -78,33 +78,33 @@
     // Function to hide the cursor when the mouse leaves the window
     function hideCursor() {
         const body = document.querySelector('body');
-        body.addEventListener('mouseleave', function() {
+        body.addEventListener('mouseleave', function () {
             circles.forEach(function (circle) {
                 circle.style.opacity = 0;
             });
         });
-        body.addEventListener('mouseenter', function() {
+        body.addEventListener('mouseenter', function () {
             circles.forEach(function (circle) {
                 circle.style.opacity = 1;
             });
         });
     }
-    
+
     hideCursor();
-    
+
     function animateCircles() {
         let x = coords.x;
         let y = coords.y;
-        
+
         circles.forEach(function (circle, index) {
             circle.style.left = x - 12 + "px";
             circle.style.top = y - 12 + "px";
-            
+
             circle.style.scale = (circles.length - index) / circles.length;
-            
+
             circle.x = x;
             circle.y = y;
-            
+
             const nextCircle = circles[index + 1] || circles[0];
             x += (nextCircle.x - x) * 0.3;
             y += (nextCircle.y - y) * 0.3;
@@ -114,8 +114,8 @@
 
     // Function to clear the circles when the page is loaded
     function clearCircles() {
-        circles.forEach(function(circle) {
-            circle.style.opacity=0;
+        circles.forEach(function (circle) {
+            circle.style.opacity = 0;
         })
     }
 
@@ -126,22 +126,22 @@
 /* ----- Smooth Scrolling ----- */
 
 {
-    $(document).ready(function(){
-        $("a").on('click', function(event) {
-            
+    $(document).ready(function () {
+        $("a").on('click', function (event) {
+
             // Make sure this.hash has a value before overriding default behavior
             if (this.hash !== "") {
                 // Prevent default anchor click behavior
                 event.preventDefault();
-                
+
                 // Store hash
                 var hash = this.hash;
-                
+
                 // Using jQuery's animate() method to add smooth page scroll
                 // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
-                }, 860, function(){
+                }, 860, function () {
                     // Add hash (#) to URL when done scrolling (default click behavior)
                     window.location.hash = hash;
                 });
