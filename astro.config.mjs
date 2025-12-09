8cf3ca7a0544bd3bcbda26999ac6a5300ef704ea
Rakesh9100 <roshanrakesh7362@gmail.com>
2025-12-09 23:18:49 +0530

    SEO improved and added 404 error page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://rakeshroshan.netlify.app',
   integrations: [tailwind()],
   output: 'static',
   devToolbar: {
