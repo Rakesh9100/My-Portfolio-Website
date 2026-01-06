01f0fe8eb25ec18eb8e4ab56a6df10468f347df2
Rakesh9100 <roshanrakesh7362@gmail.com>
2026-01-06 10:56:00 +0530

    Redesigned the website with graphics and performance optimizations

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,53 @@
 /// <reference types="astro/client" />
+/// <reference types="astro/env" />
+
+// Astro assets module declaration
+declare module 'astro:assets' {
+    export interface ImageMetadata {
+        src: string;
+        width: number;
+        height: number;
+        format: string;
+    }
+
+    export const Image: any;
+    export const Picture: any;
+    export const getImage: any;
+}
+
+// Image file declarations
+declare module '*.png' {
+    const src: ImageMetadata;
+    export default src;
+}
+
+declare module '*.jpg' {
+    const src: ImageMetadata;
+    export default src;
+}
+
+declare module '*.jpeg' {
+    const src: ImageMetadata;
+    export default src;
+}
+
+declare module '*.webp' {
+    const src: ImageMetadata;
+    export default src;
+}
+
+declare module '*.svg' {
+    const src: ImageMetadata;
+    export default src;
+}
+
+// React Fragment for JSX
+declare global {
+    const Fragment: any;
+}
+
+declare namespace JSX {
+    interface IntrinsicElements {
+        [elemName: string]: any;
+    }
+}
